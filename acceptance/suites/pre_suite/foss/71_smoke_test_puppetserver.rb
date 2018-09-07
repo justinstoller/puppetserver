6b40b8eeb735ada5fc15630825145bda3a670e16
Justin Stoller <justin.stoller@gmail.com>
2018-09-07 09:19:43 -0700

    hack presuite

--- acceptance/suites/pre_suite/foss/71_smoke_test_puppetserver.rb
+++ acceptance/suites/pre_suite/foss/71_smoke_test_puppetserver.rb
@@ -12,6 +12,15 @@ step "(SERVER-414) Make sure puppetserver can start without puppet resource, "\
   "apply, or agent affecting the known good state of the SUT in a way that "\
   "causes the tests to pass with false positive successful results."
 
+on master, 'yum install -y git'
+on master, 'git clone git://github.com/justinstoller/puppetserver-ca-cli.git ' +
+           '-b server2304-honor-existing-master-keys /opt/puppetserver-ca'
+on master, 'pushd /opt/puppetserver-ca; ' +
+           'sed -i "s,0.5.0,0.5.1," /opt/puppetserver-ca/lib/puppetserver/ca/version.rb; ' +
+           '/opt/puppetlabs/puppet/bin/gem build puppetserver-ca.gemspec; ' +
+           '/opt/puppetlabs/puppet/bin/gem install /opt/puppetserver-ca/puppetserver-ca-0.5.1.gem; ' +
+           'popd'
+
 variant = master['platform'].to_array.first
 case variant
   when /^(redhat|el|centos)$/
