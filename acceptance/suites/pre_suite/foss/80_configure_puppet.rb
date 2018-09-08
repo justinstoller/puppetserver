983cd8d685de7c31a103b05caefd147ce2b167fe
Justin Stoller <justin.stoller@gmail.com>
2018-09-08 11:21:23 -0700

    (maint) allow signing certs with SANs

--- acceptance/suites/pre_suite/foss/80_configure_puppet.rb
+++ acceptance/suites/pre_suite/foss/80_configure_puppet.rb
@@ -6,4 +6,7 @@ step "Configure puppet.conf" do
   lay_down_new_puppet_conf( master,
                            {"main" => { "dns_alt_names" => "puppet,#{hostname},#{fqdn}",
                                        "verbose" => true }}, dir)
+
+  on master, %q{/opt/puppetlabs/puppet/bin/ruby -e 'require "hocon/parser/config_document_factory"; file = "/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf"; o = Hocon::Parser::ConfigDocumentFactory.parse_file(file); n = o.set_value("certificate-authority.allow-subject-alt-names", "true"); File.write(file, n.render)'}
+
 end
